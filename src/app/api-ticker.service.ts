d3bc2b6fcb558448996cefc6958b86ae58074a7b
imasr <ashishs723@gmail.com>
2019-01-06 15:07:47 +0530

    angular update 5.2 to 7.1.4

--- src/app/api-ticker.service.ts
+++ src/app/api-ticker.service.ts
@@ -1,12 +1,11 @@
+
+import {throwError as observableThrowError,  Observable ,  Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { environment } from './../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/throw';
+import { map, catchError } from 'rxjs/operators';
+
 import * as _ from 'lodash';
-import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class ApiTickerService {
@@ -14,40 +13,40 @@ export class ApiTickerService {
   private childMethodCall = new Subject<any>();
   componentMehtodCalled$ = this.childMethodCall.asObservable();
   currencies(data): Observable<any> {
-    return this.httpIntercept.get(environment.apiUrl + `ticker/${data}/`)
-        .map(res => {
+    return this.httpIntercept.get(environment.apiUrl + `ticker/${data}/`).pipe(
+        map(res => {
             this.childMethodCall.next(res);
             return res;
-        })
-        .catch(err => {
-            return Observable.throw(err || 'Server error');
-        });
+        }),
+        catchError(err => {
+            return observableThrowError(err || 'Server error');
+        }));
   }
   specificCurrency(currencyName): Observable<any> {
-    return this.httpIntercept.get(environment.apiUrl + `ticker/${currencyName}/`)
-        .map(res => {
+    return this.httpIntercept.get(environment.apiUrl + `ticker/${currencyName}/`).pipe(
+        map(res => {
             return res;
-        })
-        .catch(err => {
-            return Observable.throw(err || 'Server error');
-        });
+        }),
+        catchError(err => {
+            return observableThrowError(err || 'Server error');
+        }));
   }
   chart(currencyName) {
-       return this.httpIntercept.get(`https://graphs2.coinmarketcap.com/currencies/${currencyName}/`)
-        .map(res => {
+       return this.httpIntercept.get(`https://graphs2.coinmarketcap.com/currencies/${currencyName}/`).pipe(
+        map(res => {
             return res;
-        })
-        .catch(err => {
-            return Observable.throw(err || 'Server error');
-        });
+        }),
+        catchError(err => {
+            return observableThrowError(err || 'Server error');
+        }));
   }
   globalData() {
-        return this.httpIntercept.get(environment.apiUrl + `global/`)
-        .map(res => {
+        return this.httpIntercept.get(environment.apiUrl + `global/`).pipe(
+        map(res => {
             return res;
-        })
-        .catch(err => {
-            return Observable.throw(err || 'Server error');
-        });
+        }),
+        catchError(err => {
+            return observableThrowError(err || 'Server error');
+        }))
     }
 }
--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
+import { Observable } from 'rxjs';
+
 
 
 @Injectable()
